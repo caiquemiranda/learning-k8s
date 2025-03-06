c409b980ad41505ef06681cf0054e1f94b1a7fe6
caiquemiranda <caiquemirandas@gmail.com>
2025-03-05 23:02:01 -0300

    Update main.ts to use consistent quotation marks and add /filmes endpoint; create filmes.ts for movie data

--- k8s-projeto1-app-base-main/backend/src/main.ts
+++ k8s-projeto1-app-base-main/backend/src/main.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express from "express"
 import { configDotenv } from "dotenv"
 
 configDotenv()
@@ -6,10 +6,14 @@ configDotenv()
 const app = express()
 const porta = process.env.PORTA
 
-app.get('/ping', (req, res) => {
-  res.send('pong!')
+app.get("/ping", (req, res) => {
+  res.send("pong!")
 })
 
 app.listen(porta, () => {
   console.log(`Server rodando porta: ${porta}`)
+})
+
+app.get("/filmes", (req, res) => {
+  res.json()
 })
